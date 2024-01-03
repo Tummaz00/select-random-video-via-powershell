764ab9103f09a61633689778a7a7488e8244d444
Tummaz00 <45412626+Tummaz00@users.noreply.github.com>
2024-01-03 18:11:31 -0500

    Upload Completed Script
    Script will open a random video within a folder filled with videos

---
+++ SelectRandomVideoViaPS.ps1
@@ -0,0 +1,17 @@
+﻿<#
+
+This script will grab a random video from a folder and play it
+
+#>
+
+$vidFormats = @("*.mp4")
+$vidFolder = "C:\Users\tamma\Downloads\For_IG";
+
+function runRandomVideo(){
+    gci $vidFolder -include $vidFormats -recurse | Get-Random -Count 1 | Invoke-Item;
+}
+
+runRandomVideo;
+
+#the line below is not needed because Invoke-Item makes video commit's default action (which is play)
+#start "$vidFolder\Damn.mp4";
